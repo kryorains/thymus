60e2542d37e08d6b13b8fcad8e05551e0fcccac6
kryorains <kryorains@kryorains.io>
2024-06-17 19:02:14 +0800

    🎉 Feat: Init commit.

---
+++ src/main/java/io/kryorains/server/thymus/ThymusApplication.java
@@ -0,0 +1,13 @@
+package io.kryorains.server.thymus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymusApplication.class, args);
+    }
+
+}
